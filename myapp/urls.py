730b8c6328e151087cb530cb49dc2fb2ef39eb98
unknown <sachinkn2019@gmail.com>
2020-08-12 22:52:02 +0530

    first commit

---
+++ myapp/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from myapp import views
+app_name="myapp"
+
+urlpatterns = [
+    path('trail/',views.trail,name="Trail"),
+    path('profile/',views.profile,name="profile"),
+    path('get_demo/',views.get_demo,name="get_demo"),
+    path('post_demo/',views.post_demo,name="post_demo"),
+    path('register/',views.register,name="register"),
+    path('multiselect/',views.multiselect,name="multiselect"),
+    path('img/',views.img_upld,name="img"),
+]
---
+++ myapp/views.py
@@ -0,0 +1,64 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.core.mail import send_mail
+
+# Create your views here.
+def trail(request):
+    return HttpResponse("<h1>project is on air</h1>")
+
+def base(request):
+    return render(request,"base.html")
+
+def home(request):
+    return render(request,"myapp/home.html")    
+
+def profile(request):
+    name="sachin"
+    return render(request,"myapp/profile.html",{'name':name})
+
+def get_demo(request):
+    name=request.GET.get('name')
+    return render(request,"get_demo.html",{'name':name})
+
+def post_demo(request):
+    if request.method=="POST":
+        name=request.POST.get('name')
+        return HttpResponse("<h1>Thanks for submission Mr./Ms. {}</h1>".format(name))
+    return render(request,"post_demo.html") 
+
+def register(request):
+    if request.method=="POST":
+        first_name=request.POST.get("first_name")
+        second_name=request.POST.get("second_name")
+        email=request.POST.get("email")
+        password=request.POST.get("pwd")
+        phno=request.POST.get("phno")
+        date=request.POST.get("birthday_day")
+        month=request.POST.get("birthday_month")
+        year=request.POST.get("birthday_year")
+        gender=request.POST.get("sex")
+        if gender=="1":
+            gender="Female"
+        else:
+            gender="Male"
+
+        send_mail("Thanks for Registration","hello Mr./Ms.{} {}\n Thanks for Registering".format(first_name,second_name),
+        "sachinkn2017@gmail.com" ,[email,],fail_silently=True)
+        return HttpResponse("{}<br>{}<br>{}<br>{}<br>{}<br>{}<br>{}<br>{}<br>{}<br>".format(first_name,second_name,email,password,phno,gender,date,month,year))
+    return render(request,"myapp/register.html")
+
+
+def multiselect(request):
+    if request.method=="POST":
+        foods=request.POST.getlist("food")
+        languages=request.POST.getlist("language")
+        return HttpResponse("<h1>{}{}<h1>".format(foods,languages))
+    return render(request,'multiselect.html')
+
+def img_upld(request):
+    if request.method=="POST" and request.FILES:
+        image=request.FILES['sam']
+        fs=FileSystemStorage()
+        fs.save(image.name,image)
+
+    return render(request,"img_upld.html")            
